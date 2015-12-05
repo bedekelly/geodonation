41bbb951266be6a043ee6d91e5764cc04e2b4829
Peran Truscott <darthperagon@gmail.com>
2015-12-05 19:20:52 +0000

    Add map.jsand index.html

--- loc/static/js/map.js
+++ loc/static/js/map.js
@@ -1,24 +1,24 @@
-var markersPos = [];
+var markerPos = [];
 
-var myPos = {lat: 53.462, lng: -2.228}
-markerPos.push(myPos);
-myPos = {lat: 54.462, lng: -2.228}
-markerPos.push(myPos);
-var map;
+      var myPos = {lat: 53.462, lng: -2.228}
+      markerPos.push(myPos);
+      myPos = {lat: 53.662, lng: -2.228}
+      markerPos.push(myPos);
+      var map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myPos,
-    zoom: 15
-  });
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: myPos,
+          zoom: 15
+        });
 
-  for (var i = 0; i < markerPos.length; i++) {
-    var marker = new google.maps.Marker({
-      position: markerPos[i],
-      map: map,
-      title: 'My position'
-    });        
-  }
-}    
+        for (var i = 0; i < markerPos.length; i++) {
+          var marker = new google.maps.Marker({
+            position: markerPos[i],
+            map: map,
+            title: 'Hello World!'
+          });        
+        }
+      }     
 
 initMap();
